ba00954d77a412858e2d16130330ad0ba6a0deea
Bobokhuja <bobokhuja@yandex.ru>
2024-01-16 11:06:21 +0500

    Install react router dom

--- src/app/providers/providers.tsx
+++ src/app/providers/providers.tsx
@@ -1,13 +1,16 @@
 import { PropsWithChildren } from 'react'
 import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
 import { NextUIProvider } from '@nextui-org/react'
 
 import { store } from '@app/store'
 
 export const Providers = ({ children }: PropsWithChildren) => {
   return (
-    <Provider store={store}>
-      <NextUIProvider>{children}</NextUIProvider>
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <NextUIProvider>{children}</NextUIProvider>
+      </Provider>
+    </BrowserRouter>
   )
 }
