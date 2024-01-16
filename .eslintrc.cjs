982cd49d9f08689df783205af7a174907b8a741c
Bobokhuja <bobokhuja@yandex.ru>
2024-01-16 09:28:12 +0500

    Configuration ESLint
    Configuration ESLint with eslint-kit.
    Install dependencies and config prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,29 @@
-module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+const { configure, presets } = require('eslint-kit')
+
+module.exports = configure({
+  allowDebug: process.env.NODE_ENV !== 'production',
+  presets: [
+    presets.imports({
+      sort: {
+        newline: true,
+        groups: [
+          // react and other external imports
+          ['^react', '^[^.\@\\/]', '^@?\\w'],
+
+          // common aliases (@app, @root, @/, ~/) and anything not matched
+          ['@app', '@pages', '@widgets', '@features', '@entities', '@shared'],
+
+          // relative imports
+          ['^\\.'],
+
+          // side effects
+          ['^\\u0000'],
+        ],
+      },
+    }),
+    presets.node(),
+    presets.prettier(),
+    presets.typescript(),
+    presets.react(),
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
-}
+})
