16a179039132c5d5ee83314c863eb8205fabe785
Bobokhuja <bobokhuja@yandex.ru>
2024-01-19 10:18:00 +0500

    Configuration svgr in vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite'
-import viteTsconfigPaths from 'vite-tsconfig-paths'
 import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
+import svgr from 'vite-plugin-svgr'
+import viteTsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteTsconfigPaths()],
+  plugins: [
+    react(),
+    viteTsconfigPaths(),
+    svgr({
+      include: '**/*.svg',
+    }),
+  ],
 })
